f6a66183f932ab3a9f3e668be4b60377e253d39e
RubinaathKP <rubinaath612@gmail.com>
2025-10-02 22:17:56 +0530

    final

--- src/components/ThingsILikeCarousel.jsx
+++ src/components/ThingsILikeCarousel.jsx
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
 import styles from './ThingsILikeCarousel.module.css';
-
+import calm from './calm.png';
+import smile from './smile.png';
+import french from './french.png' // Adjust to the actual image path
 const cardsData = [
   {
     title: "Votre calme",
     subtitle: "Your Calmness",
     description: "You are the Calmness in the Storm, a serene presence that soothes and inspires.",
-    imgSrc: "/images/anime_heart.png",
+    imgSrc: {calm},
     imgAlt: "Calmness in the Storm",
   },
   {
     title: "Ta Grâce Française",
     subtitle: "Your French Grace",
     description: "Your elegance and charm are as timeless as the streets of Paris.",
-    imgSrc: "/images/french_grace.png",
+    imgSrc: {french},
     imgAlt: "French Grace",
   },
   {
     title: "Ton Sourire Radieux",
     subtitle: "Your Radiant Smile",
     description: "Your smile lights up every room and warms every heart.",
-    imgSrc: "/images/radiant_smile.png",
+    imgSrc: {smile},
     imgAlt: "Radiant Smile",
   },
   {
     title: "Vos discussions",
     subtitle: "Your Chats",
     description: "Your chatty nature brings joy and laughter to those around you especially me.",
-    imgSrc: "/images/kindness.png",
+    imgSrc: {chat},
     imgAlt: "Kindness",
   },
 ];
